a97ea270b23f1483411d532373c0500904d2ede3
Ann-JY <78674600+Ann-JY@users.noreply.github.com>
2024-05-11 00:39:42 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   // needed for github pages just put the repo name here
-  base: "/d3-basic-scatterplot/",
+  base: '/d3-week9/', 
 });
